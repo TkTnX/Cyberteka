6881b92e837a82eea81724bd01cfc5eefe6c3953
TkTnX <timur20-09@mail.ru>
2026-05-27 00:22:08 +0500

    add: адаптив главной

--- src/widgets/Navigation/Navigation.tsx
+++ src/widgets/Navigation/Navigation.tsx
@@ -1,24 +1,30 @@
 import { useLocation } from "react-router";
 import { NAVIGATION } from "../../shared";
 import "./_navigation.scss";
+import { Menu } from "lucide-react";
 
 export const Navigation = () => {
   const location = useLocation();
 
   return (
-    <nav className="navigation">
-      <ul className="navigation__list">
-        {NAVIGATION.map((item) => (
-          <li className="navigation__item" key={item.href}>
-            <a
-              href={item.href}
-              className={`navigation__link ${location.pathname === item.href && "navigation__link--active"}`}
-            >
-              {item.label}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav className="navigation">
+        <ul className="navigation__list">
+          {NAVIGATION.map((item) => (
+            <li className="navigation__item" key={item.href}>
+              <a
+                href={item.href}
+                className={`navigation__link ${location.pathname === item.href && "navigation__link--active"}`}
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button className="navigation__button">
+        <Menu strokeWidth={3} size={50} />
+      </button>
+    </>
   );
 };
--- src/widgets/Sales/Sales.tsx
+++ src/widgets/Sales/Sales.tsx
@@ -1,14 +1,17 @@
 import { Sale } from "../../entities";
-import { SALES } from "../../shared";
+import { Button, SALES } from "../../shared";
 import "./_sales.scss";
 export const Sales = () => {
   return (
     <section className="sales">
       <div className="sales__content container">
         <h2 className="sales__title">АКЦИЙ И СКИДКИ НА ЛЮБОЙ ВКУС</h2>
-              <div className="sales__list">
-                  {SALES.map((sale) => <Sale key={sale.id} sale={sale} />)}
+        <div className="sales__list">
+          {SALES.map((sale) => (
+            <Sale key={sale.id} sale={sale} />
+          ))}
         </div>
+        <Button className="sales__button" text="ЕЩЕ АКЦИИ →" />
       </div>
     </section>
   );
